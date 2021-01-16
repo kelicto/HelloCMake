e714fb2bc51bbfc0aa10a3e0ca55ffadfe98f61b
KeLi <kelicto@protonmail.com>
2021-01-17 01:33:12 +0800

    Managed custom resource.

--- Src/KeLi.HelloCMake.WinFormApp/Properties/Resources.Designer.cs
+++ Src/KeLi.HelloCMake.WinFormApp/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace KeLi.HelloCMake.WinFormApp.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    internal class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace KeLi.HelloCMake.WinFormApp.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
+        internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KeLi.HelloCMake.WinFormApp.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace KeLi.HelloCMake.WinFormApp.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture {
+        internal static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,9 +63,19 @@ namespace KeLi.HelloCMake.WinFormApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap _001 {
+        internal static System.Drawing.Bitmap _001 {
             get {
                 object obj = ResourceManager.GetObject("_001", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _002 {
+            get {
+                object obj = ResourceManager.GetObject("_002", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
